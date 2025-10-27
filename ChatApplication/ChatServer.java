4cc755ce7239c86e829d5932fc7256e28dc7a425
Priyanka B <priyankabasavaraju2000@gmail.com>
2025-10-27 17:25:24 +0530

    Add files via upload

---
+++ ChatApplication/ChatServer.java
@@ -0,0 +1,67 @@
+package ChatApp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        PrintWriter output = null;
+        BufferedReader userInput = null;
+
+        try {
+            serverSocket = new ServerSocket(5000);
+            System.out.println("Server started. Waiting for clients......");
+
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket);
+
+            BufferedReader input = new BufferedReader((new InputStreamReader(clientSocket.getInputStream())));
+            output = new PrintWriter(clientSocket.getOutputStream(),true);
+
+            Thread readThread = new Thread(()-> {
+                String message;
+                try {
+                    while((message = input.readLine()) != null) {
+                        System.out.println("User: " + message);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            readThread.start();
+
+            userInput = new BufferedReader(new InputStreamReader(System.in));
+            String message;
+            while ((message = userInput.readLine()) != null) {
+                output.println(message);
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            output.close();
+            try {
+                userInput.close();
+                clientSocket.close();
+                serverSocket.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
